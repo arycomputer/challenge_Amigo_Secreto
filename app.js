735b378b4ffec3e94388701703ee7b703b96f0b6
arycomputer <33355206+arycomputer@users.noreply.github.com>
2025-03-09 00:53:22 -0300

    update

--- app.js
+++ app.js
@@ -234,7 +234,19 @@ function showModal(mensagem) {
     }
 }
 
+window.onscroll = function () { myFunction() };
 
+//var header = document.getElementById("header");
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
 
 
 
